fa4f784f00fd4337f8bd71ecf9ac614dfed60cbb
William Arnback <32479398+Wlammm@users.noreply.github.com>
2023-03-24 21:55:31 +0100

    removed unecessary imgui folders

--- Engine/Build/update_git_submodules.bat
+++ Engine/Build/update_git_submodules.bat
@@ -6,4 +6,11 @@ echo Updating submodules from github...
 git submodule init
 git submodule update
 echo Update successful
+
+cd Engine/imgui
+rmdir /s /q examples
+rmdir /s /q docs
+rmdir /s /q backends
+rmdir /s /q misc
+
 PAUSE
